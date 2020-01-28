a1229f0a0d27692c0ff488e889c9f0cdb958764b
RiverRobins <rivergrahamrobins@gmail.com>
2020-01-28 15:38:33 -0600

    finished views basic html

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -14,18 +14,41 @@ public class PostController {
 //    @ResponseBody
     public String posts(Model model) {
         ArrayList<Post> temp = new ArrayList<>();
-        temp.add(new Post(1L,1L,"title","body text", new ArrayList<Comment>()));
+        ArrayList<Comment> coms = new ArrayList<>();
+        coms.add(new Comment(1L, 1L, "this is a comment"));
+        coms.add(new Comment(1L, 1L, "this is a comment"));
+        coms.add(new Comment(1L, 1L, "this is a comment"));
+        coms.add(new Comment(1L, 1L, "this is a comment"));
+        coms.add(new Comment(1L, 1L, "this is a comment"));
+        coms.add(new Comment(1L, 1L, "this is a comment"));
+        temp.add(new Post(1L,1L,"title","body text", coms));
         temp.add(new Post(1L,1L,"title 2","body text 2", new ArrayList<Comment>()));
         temp.add(new Post(1L,1L,"title 3","body text 3", new ArrayList<Comment>()));
         temp.add(new Post(1L,1L,"title 4","body text 4", new ArrayList<Comment>()));
         model.addAttribute("posts", temp);
+
+
         return "posts/index";
     }
 
     @GetMapping(path = "/posts/{id}")
-    @ResponseBody
-    public String spec(@PathVariable String id) {
-        return "post(singular) with ID of " + id;
+    public String post(@PathVariable String id, Model model) {
+        ArrayList<Post> temp = new ArrayList<>();
+        ArrayList<Comment> coms = new ArrayList<>();
+        coms.add(new Comment(1L, 1L, "this is a comment"));
+        coms.add(new Comment(1L, 1L, "this is a comment"));
+        coms.add(new Comment(1L, 1L, "this is a comment"));
+        coms.add(new Comment(1L, 1L, "this is a comment"));
+        coms.add(new Comment(1L, 1L, "this is a comment"));
+        coms.add(new Comment(1L, 1L, "this is a comment"));
+        temp.add(new Post(1L,1L,"title","body text", coms));
+        temp.add(new Post(2L,1L,"title 2","body text 2", new ArrayList<Comment>()));
+        temp.add(new Post(3L,1L,"title 3","body text 3", new ArrayList<Comment>()));
+        temp.add(new Post(4L,1L,"title 4","body text 4", new ArrayList<Comment>()));
+        model.addAttribute("post", temp.get(Integer.parseInt(id) - 1));
+
+
+        return "posts/test";
     }
 
     @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
