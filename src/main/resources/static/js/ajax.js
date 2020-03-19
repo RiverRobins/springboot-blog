f1e514bc600bb516518e7895515af9ac337e7d49
RiverRobins <rivergrahamrobins@gmail.com>
2020-03-19 16:08:15 -0500

    working on ajax stuff

--- src/main/resources/static/js/ajax.js
+++ src/main/resources/static/js/ajax.js
@@ -1,15 +1,43 @@
-$(".like-button").addEventListener().click(function () {
-    $.ajax( "/posts/" + this.getPropertyValue("id").substr(5) + "/like", {
+$(".like-button").on("click", function () {
+    // console.dir($(this).parent().children()[0]);
+    const id = $(this).parent().children()[0].value;
+    const from = $(this).parent().children()[1].value;
+
+    console.log("post #" + id); //returns correct output
+    console.log("from page " + from); //returns correct output
+
+    $.ajax( "/posts/" + $(this).parent().children()[0].value + "/like", {
         type: "POST",
         data: {
-            post: this.getPropertyValue("id").substr(5),
-            from: this.getPropertyValue("from")
+            post: id,
+            from: from
         },
         success: function (data, status, xhr) {
-
+            console.log("post liked!");
         },
-        error: function () {
+        // error: function (e) {
+        //     console.log(e)
+        // }
 
-        }
     });
+
+    // console.log("post #" + id);
+    // console.log("from page " + from);
+
+
 });
+// $(".like-button").addEventListener().click(function () {
+//     $.ajax( "/posts/" + this.getPropertyValue("id").substr(5) + "/like", {
+//         type: "POST",
+//         data: {
+//             post: this.getPropertyValue("id").substr(5),
+//             from: this.getPropertyValue("from")
+//         },
+//         success: function (data, status, xhr) {
+//
+//         },
+//         error: function () {
+//
+//         }
+//     });
+// });
