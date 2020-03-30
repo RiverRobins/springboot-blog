c20c5575758082e97973c626ccad1634d2187433
RiverRobins <rivergrahamrobins@gmail.com>
2020-03-30 15:29:55 -0500

    cleaned code in follows and removed unused file

--- src/main/java/com/codeup/springblog/models/Follow.java
+++ src/main/java/com/codeup/springblog/models/Follow.java
@@ -5,16 +5,11 @@ import javax.persistence.*;
 @Entity
 public class Follow {
 
-//    @EmbeddedId
-//    private FollowKey followKey;
-
     @ManyToOne
-//    @MapsId("follower_id")
     @JoinColumn(name = "follower_id", insertable=false, updatable=false)
     private User follower;
 
     @ManyToOne
-//    @MapsId("following_id")
     @JoinColumn(name = "following_id", insertable=false, updatable=false)
     private User following;
 
@@ -22,20 +17,6 @@ public class Follow {
         this.follower = follower;
         this.following = following;
     }
-
-    //    public Follow(FollowKey followKey, User follower, User following) {
-//        this.followKey = followKey;
-//        this.follower = follower;
-//        this.following = following;
-//    }
-//
-//    public FollowKey getFollowKey() {
-//        return followKey;
-//    }
-//
-//    public void setFollowKey(FollowKey followKey) {
-//        this.followKey = followKey;
-//    }
 
     public User getFollower() {
         return follower;
--- src/main/java/com/codeup/springblog/models/User.java
+++ src/main/java/com/codeup/springblog/models/User.java
@@ -150,7 +150,6 @@ public class User {
 //        return all;
 //    }
 
-
     public List<Follow> getFollowing() {
         return following;
     }
